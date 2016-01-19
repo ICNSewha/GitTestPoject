ca76976e47436b3c4e3d3134ad6058e962cf84ef
ICNSewha <yangzzzzz@ewhain.net>
2016-01-19 18:25:46 +0900

    change code by dana

--- GitTestProject.cpp
+++ GitTestProject.cpp
@@ -1,15 +1,20 @@
-// Do not remove the include below
+/*
+ * Created By DANA
+ * 2016-01-19
+ */
+
 #include "GitTestProject.h"
 
+int num =0;
 
-//The setup function is called once at startup of the sketch
 void setup()
 {
-// Add your initialization code here
+	if(num == 0) false;
+	else true;
 }
 
-// The loop function is called in an endless loop
 void loop()
 {
-//Add your repeated code here
+
 }
+
